f03a08da647dcc10b8eede7290850d8bc0a69082
Alex Kozlenkov <alex.kozlenkov@googlemail.com>
2010-04-14 15:40:48 +0000

    RESOLVED - issue PROVA-41: Add subscription leasing to the engine's OSGi service http://prova.ws/jira/browse/PROVA-41

--- src/main/java/ws/prova/reference2/messaging/ProvaServiceMessageImpl.java
+++ src/main/java/ws/prova/reference2/messaging/ProvaServiceMessageImpl.java
@@ -1,6 +1,9 @@
 package ws.prova.reference2.messaging;
 
+import java.util.Map;
+
 import ws.prova.agent2.ProvaReagent;
+import ws.prova.kernel2.ProvaConstant;
 import ws.prova.kernel2.ProvaList;
 import ws.prova.reference2.ProvaConstantImpl;
 import ws.prova.service.ProvaService;
@@ -9,22 +12,36 @@ public class ProvaServiceMessageImpl implements ProvaDelayedCommand {
 
 	private String dest;
 	
-	private ProvaList terms;
-
 	private ProvaService service;
+
+	private String xid;
+
+	private Object payload;
+
+	private String agent;
+
+	private String verb;
 	
 	public ProvaServiceMessageImpl(String dest, ProvaList terms,
 			String agent, ProvaService service) {
+		this.xid = terms.getFixed()[0].toString();
 		this.dest = dest;
-		this.terms = terms;
-		this.terms.getFixed()[2] = ProvaConstantImpl.create(agent);
+		this.agent = agent;
+		this.verb = terms.getFixed()[3].toString();
+		this.payload = terms.getFixed()[4];
+		if( this.payload instanceof ProvaConstant && ((ProvaConstant) this.payload).getObject() instanceof Map<?, ?> )
+			this.payload = ((ProvaConstant) payload).getObject();
+		else {
+			terms.getFixed()[2] = ProvaConstantImpl.create(agent);
+			this.payload = terms;
+		}
 		this.service = service;
 	}
 
 	@Override
 	public void process(ProvaReagent prova) {
 		try {
-			service.send(dest, terms);
+			service.send(xid, dest, agent, verb, payload);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
