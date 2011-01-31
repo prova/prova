5b2f5b89be24702041cfcbdfb59958007367cc33
Kia Teymourian <kia@fu-berlin.de>
2011-01-31 15:22:59 +0000

    added a bash script for running prova on linux mashines

---
+++ prova3.sh
@@ -0,0 +1,11 @@
+CP=$CLASSPATH:target/ws.prova.compact-3.0.1.jar
+
+for j in lib; do
+    for i in `ls $j`; do
+	CP=$CP:$j/$i;
+    done
+done
+
+# java -jar platform/plugins/org.eclipse.osgi_3.5.2.R35x_v20100126.jar -console
+
+java -Xms512m -Xmx512m -XX:NewSize=256m -XX:MaxNewSize=256m -cp $CP ws.prova.api2.ProvaCommunicatorImpl prova password $1
